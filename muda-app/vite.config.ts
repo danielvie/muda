c0c9e5d778459b572f428b68b08535b8a6b55e15
Almeida, Heitor A <heitor.a.almeida@boeing.com>
2025-12-10 09:54:00 -0300

    app first commit

---
+++ muda-app/vite.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Muda APP',
+        short_name: 'muda',
+        description: 'Muda',
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: 'icons/128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: "any"
+          },
+          {
+            src: 'icons/192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: "any"
+          },
+          {
+            src: 'icons/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: "any maskable"
+          },
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+
+    })
+  ],
+})
+
+ 
